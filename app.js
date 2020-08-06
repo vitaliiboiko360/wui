00fe2cee6e38d3c4f8fc38ca2763a80e90cb0c11
vitalii boiko <vukelan@gmail.com>
2020-08-06 20:22:52 +0000

    publish svg html

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const http = require('http');
+const fs = require('fs');
 
 const hostname = '192.168.129.131';
 const port = 8080;
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('message to the client');
+    res.setHeader('Content-Type', 'text/html');
+    
+    fs.readFile('./svg/circle.html', (err, data) => {
+        if (err) throw err;
+        res.end(data);
+      });
+
+    
 });
 
 server.listen(port, hostname, ()=> {
