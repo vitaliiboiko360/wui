a01de0104140f5f9744b34dc5f064daa5c5aab1b
vitalii boiko <vukelan@gmail.com>
2020-09-11 21:21:35 +0000

    add serving main index html file

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const http = require('http');
 const fs = require('fs');
+const router = require('./router.js');
 
 const hostname = '192.168.129.131';
 const port = 8080;
 
 const server = http.createServer((req, res) => {
-    onRequest(req, res);    
+    router(req, res);    
 });
 
 server.listen(port, hostname, ()=> {
--- router.js
+++ router.js
@@ -1,3 +1,19 @@
+var fs = require('fs');
+var http = require('http');
 
-export onRequest = onRequest;
 
+function router (request, response) {
+    if(request.url == "index.html") {
+        fs.readFile(__dirname + "index.html", function(err, data) {
+            if (err) {
+                response.writeHeader(404);
+                response.end(JSON.stringify(err));
+                return;
+            }
+            response.writeHead(200);
+            response.end(data);
+        })
+    }
+}
+
+exports.router = router;
